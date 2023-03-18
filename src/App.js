aa65e20d866e0be121b9f32810422623cfe551bf
aniljayswal8685 <93642023+aniljayswal8685@users.noreply.github.com>
2023-03-18 09:13:54 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ export default function App() {
 
   function handleinput(){
     console.warn("function call")
-    inputRef.current.value="1000"
+    // inputRef.current.value="1000"
+    // inputRef.current.focus();
+    inputRef.current.style.color="red"
+
   }
   return (
     <div>
       <h1>UseRef in React</h1>
       <input type="text" ref={inputRef}/>
-      <button onclick={handleinput}>HandleInput</button>
+      <button onClick={handleinput}>HandleInput</button>
       
     </div>
   );
